b653e6722a35251643fa5546a66986470deaca82
R0M <36591161+R0M-0X@users.noreply.github.com>
2024-05-18 16:30:55 +0200

    Update_180520241630

--- Powershell/Windows/Win11/Win11_post_install_perso.ps1
+++ Powershell/Windows/Win11/Win11_post_install_perso.ps1
@@ -153,8 +153,8 @@ Get-AppXProvisionedPackage -Online | Where-Object DisplayName -eq $UWPApp | Remo
 }
 
 # Set PSRepository
-Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 # Installation du module mises a jour
 Install-Module PSWindowsUpdate -force
 
