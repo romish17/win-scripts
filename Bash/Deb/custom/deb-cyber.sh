e5d1e47240056524afb1df3964801376344a0f26
R0M <36591161+R0M-0X@users.noreply.github.com>
2024-06-22 13:58:18 +0200

    Update_220620241358

--- Bash/Deb/custom/deb-cyber.sh
+++ Bash/Deb/custom/deb-cyber.sh
@@ -3,20 +3,37 @@ if [ "$(id -u)" == 0 ]; then echo "Please don't run as root." >&2; exit 1; fi
 INSTALL_PATH=$(pwd)
 apt update
 sudo apt install sudo git curl unzip snap snapd fonts-roboto-unhinted fonts-inconsolata -yy
-sudo apt install xfce4 -yy
+sudo apt install xfce4 xfce-terminal paper-icon-theme -yy
 sudo apt install xserver-xorg-input-multitouch xserver-xorg-input-synaptics -yy
 
-git clone https://github.com/R0M-0X/dracula-theme.git
+sudo apt install ftp python3 python3-pip nmap unzip python3-venv ssh \
+proxychains4 freerdp2-x11 lightdm lightdm-settings slick-greeter libpcap-dev \
+wget libssl-dev tcpdump smbclient imagemagick ghostscript \
+simplescreenrecorder libcurl4-openssl-dev libssl-dev jq \
+mariadb-client dirb whois sslsplit dnsrecon sipsak braa onesixtyone \
+sipvicious build-essential make gcc fontconfig fonts-powerline \
+wireshark vlc smbmap neofetch conky apache2 mariadb-server \
+php net-tools htop gnupg2 wget gpg apt-transport-https code \
+software-properties-common google-chrome-stable golang -yy
 
-sudo cp -r assets /usr/share/
-sudo cp -r assets/Nordic-standard-buttons-v40 /usr/share/themes/
-sudo cp -r assets/wallpapers /usr/share/
-sudo cp -rf assets/avatar /usr/share/lightdm/
+
+wget https://github.com/R0M-0X/Scripts/blob/005991c92d91f3e02be98fc74a4e840e3fa00c6a/_Assets/Deb/GTK/Nordic-darker.tar.xz
+wget https://github.com/R0M-0X/Scripts/blob/005991c92d91f3e02be98fc74a4e840e3fa00c6a/_Assets/Wallpapers/wall2.jpg
+
+tar -xvf Nordic-darker.tar.xz
+cp -r Nordic-darker-v40/ /usr/share/themes/
+sudo xfconf-query -c xsettings -p /Net/ThemeName -s Nordic-standard-buttons-v40
+sudo xfconf-query -c xfwm4 -p /general/theme -s Nordic-standard-buttons-v40
+
+mkdir -p /usr/share/wallpapers/
+sudo cp wall2.jpg /usr/share/wallpapers/
+
 
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/google.asc >/dev/null
 sudo echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/chrome.list
 #wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
+## Apps 
 snap install spotify
 snap install bitwarden
 
@@ -27,20 +44,6 @@ sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packag
 sudo rm -f packages.microsoft.gpg
 
 sudo apt update 
-
-sudo apt install google-chrome-stable -yy
-
-sudo apt install ftp python3 python3-pip nmap unzip python3-venv ssh \
-proxychains4 freerdp2-x11 lightdm lightdm-settings slick-greeter libpcap-dev \
-wget libssl-dev tcpdump smbclient imagemagick ghostscript \
-simplescreenrecorder libcurl4-openssl-dev libssl-dev jq \
-mariadb-client dirb whois sslsplit dnsrecon sipsak braa onesixtyone \
-sipvicious build-essential make gcc fontconfig fonts-powerline \
-wireshark vlc smbmap neofetch conky apache2 mariadb-server \
-php net-tools htop gnupg2 wget gpg apt-transport-https code \
-software-properties-common -yy
-
-sudo apt update && apt install paper-icon-theme -yy
 
 
 #================= Change Boot logo ========================#
@@ -61,5 +64,15 @@ sudo gsettings set org.gnome.desktop.interface icon-theme "Paper-Mono-Dark"
 sudo gsettings set org.gnome.desktop.interface cursor-theme "Paper-Mono-Dark"
 # XFCE
 xfconf-query -c xsettings -p /Net/IconThemeName -s Paper-Mono-Dark
+
+
+###### Tools Cyber
+
+go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+
+
+##### Dir
+/root/
+
 
  
